d5aa723ecddf8974cf3bd23bb3b12e670a09cd9a
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-06-02 13:25:45 -0400

    began to build out code for post api/notes to be able to get notes to render on page

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 // importing necessary files to initialize app and port
 const express = require('express');
 const path = require('path');
+const noteData = require('./db/db.json');
 
 // initializing app for use
 const app = express();
 
 //create port number
-const PORT = 8888;
+const PORT = 3388;
 
-// using middleware for the public folder 
-app.use = (express.static('public'));
+// middleware 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Read index.html
+// GET /
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+// Read notes.html
+// GET /notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
+
+// GET /api/notes
+app.get('/api/notes', (req, res) => {
+    res.json(noteData);
+});
+
+// POST api/notes
+app.post('/api/notes', (req, res) => {
+    console.log(req.body)
+});
 
 // listening for requests
 app.listen(PORT, () =>
